7419cd94eb6ee43ee284c4327208b4096574f8b4
Chamnap <chamnap.um@gmail.com>
2014-10-13 14:56:48 +0700

    modify connection

--- Connections/connection.php
+++ Connections/connection.php
@@ -4,24 +4,24 @@
 
 
 
-	/*$hostname_magazinducoin = "localhost";
+	$hostname_magazinducoin = "localhost";
 
 	$database_magazinducoin = "magasin_db";
 
 	$username_magazinducoin = "root";
 
-	$password_magazinducoin = "root";*/
+	$password_magazinducoin = "root";
 
 	
 
 	
 
-	$hostname_magazinducoin = "localhost";
+	/*$hostname_magazinducoin = "localhost";
 
 	$database_magazinducoin = "magasin3_bdd";
 
 	$username_magazinducoin = "root";
 
 	$password_magazinducoin = "vi8x0vgC";
-
+*/
 ?>
